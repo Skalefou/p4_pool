a2423c26056d4751ef42953eaef191c9b03d9c1b
Skalefou <skamsagar@gmail.com>
2024-09-26 21:57:16 +0200

    Add text

--- src/Graphic.c
+++ src/Graphic.c
@@ -3,9 +3,15 @@
 #include <SDL2/SDL_image.h>
 
 int graphicInit() {
+    SDL_Color colorText = {255, 255, 255, 255};
     const char *spritePaths[] = {
         "./resource/player1.png",
         "./resource/player2.png"
+    };
+
+    const char *textTurn[] = {
+        "Au tours du joueur 1 !",
+        "Au tours du joueur 2 !"
     };
 
     game.graphic.spriteNb = 2;
@@ -20,6 +26,26 @@ int graphicInit() {
         SDL_FreeSurface(surface);
 
         if (!game.graphic.sprite[i]) {
+            printf("SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
+            return 1;
+        }
+    }
+
+    game.graphic.font = TTF_OpenFont("./resource/font.ttf", 16);
+    if (!game.graphic.font) {
+        printf("TTF_OpenFont Error: %s\n", TTF_GetError());
+        return 1;
+    }
+
+    for (int i = 0; i < 2; i++) {
+        SDL_Surface *surface = TTF_RenderText_Solid(game.graphic.font, textTurn[i], colorText);
+        if (!surface) {
+            printf("TTF_RenderText_Solid Error: %s\n", TTF_GetError());
+            return 1;
+        }
+        game.graphic.textTurn[i] = SDL_CreateTextureFromSurface(game.window.renderer, surface);
+        SDL_FreeSurface(surface);
+        if (!game.graphic.textTurn[i]) {
             printf("SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
             return 1;
         }
@@ -40,6 +66,17 @@ void graphicDisplayGrid() {
 }
 
 void graphicClose() {
+
+    for (int i = 0; i < 2; i++) {
+        if (game.graphic.textTurn[i] != NULL) {
+            SDL_DestroyTexture(game.graphic.textTurn[i]);
+        }
+    }
+
+    if (game.graphic.font != NULL) {
+        TTF_CloseFont(game.graphic.font);
+    }
+
     for (int i = 0; i < game.graphic.spriteNb; i++) {
         if (game.graphic.sprite[i] != NULL) {
             SDL_DestroyTexture(game.graphic.sprite[i]);
--- src/Graphic.h
+++ src/Graphic.h
@@ -2,14 +2,21 @@
 #define GRAPHIC_H
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 int graphicInit();
 void graphicClose();
 void graphicDisplayGrid();
 
 typedef struct {
-  int spriteNb;
-  SDL_Texture *sprite[2];
+    // Sprites
+    int spriteNb;
+    SDL_Texture *sprite[2];
+
+    // Textes
+    TTF_Font *font;
+    SDL_Texture *textTurn[2];
+    int textTurnNb;
  } Graphic;
 
 #endif
