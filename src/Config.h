a8f53b5ce24c04cc03ea68319168546a65f2c0ea
Skalefou <skamsagar@gmail.com>
2024-09-27 01:49:46 +0200

    FIN ETAPE 1

--- src/Config.h
+++ src/Config.h
@@ -1,9 +1,20 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+enum {
+    MODE_LOCAL = 0,
+    MODE_SERVER = 1,
+    MODE_CLIENT = 2
+};
+
 typedef struct {
     int rows;
     int columns;
+    int winNumberJeton;
+
+    int modeOnline;
+    char *ip;
+    int port;
 } Config;
 
 #endif
