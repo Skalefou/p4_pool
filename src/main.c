083c72e3746859df7fd53d86babd2beba0f403cf
Skalefou <skamsagar@gmail.com>
2024-09-26 19:49:08 +0200

    Init the project

--- src/main.c
+++ src/main.c
@@ -14,53 +14,6 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    SDL_Window *win = SDL_CreateWindow("Hello SDL2", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-    if (win == NULL) {
-        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
-        IMG_Quit();
-        SDL_Quit();
-        return 1;
-    }
-
-    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (ren == NULL) {
-        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
-        SDL_DestroyWindow(win);
-        IMG_Quit();
-        SDL_Quit();
-        return 1;
-    }
-
-    SDL_Surface *bmp = IMG_Load("path/to/your/image.png");
-    if (bmp == NULL) {
-        printf("IMG_Load Error: %s\n", IMG_GetError());
-        SDL_DestroyRenderer(ren);
-        SDL_DestroyWindow(win);
-        IMG_Quit();
-        SDL_Quit();
-        return 1;
-    }
-
-    SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
-    SDL_FreeSurface(bmp);
-    if (tex == NULL) {
-        printf("SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
-        SDL_DestroyRenderer(ren);
-        SDL_DestroyWindow(win);
-        IMG_Quit();
-        SDL_Quit();
-        return 1;
-    }
-
-    SDL_RenderClear(ren);
-    SDL_RenderCopy(ren, tex, NULL, NULL);
-    SDL_RenderPresent(ren);
-
-    SDL_Delay(2000); // Afficher l'image pendant 2 secondes
-
-    SDL_DestroyTexture(tex);
-    SDL_DestroyRenderer(ren);
-    SDL_DestroyWindow(win);
     IMG_Quit();
     SDL_Quit();
     return 0;
