1683e6faa7426adf8c3dc01151e002272bc62f51
Cece-111 <cv.04@laposte.net>
2024-09-26 20:25:08 +0200

    fix giiiiiiiiiiitignore

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/user/projet/p4_pool
+# Install script for directory: /home/esgi/CLionProjects/p4_pool
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/user/projet/p4_pool/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/esgi/CLionProjects/p4_pool/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
